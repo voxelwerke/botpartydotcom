121e995391c1bc60c0dad2484a5f15a34c8ab578
Ben Nolan <bnolan@gmail.com>
2026-04-18 07:54:35 +1200

    Fix deploy

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -2,11 +2,25 @@ import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		csp: {
+			mode: 'auto',
+			directives: {
+				'default-src': ['self'],
+				'connect-src': ['self'],
+				'img-src': ['self', 'data:', 'blob:'],
+				'style-src': ['self'],
+				'font-src': ['self'],
+				'object-src': ['none'],
+				'base-uri': ['none'],
+				'form-action': ['self'],
+				'frame-ancestors': ['none']
+			}
+		}
+	},
+	vitePlugin: {
+		dynamicCompileOptions: ({ filename }) => ({ runes: !filename.includes('node_modules') })
 	}
 };
 
