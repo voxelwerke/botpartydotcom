faf574f874e7c568b846181407853f108076018f
Ben Nolan <bnolan@gmail.com>
2026-04-18 08:37:45 +1200

    Disable CSP temporarily

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -4,21 +4,21 @@ import adapter from '@sveltejs/adapter-node';
 const config = {
 	kit: {
 		adapter: adapter(),
-		csp: {
-			mode: 'auto',
-			directives: {
-				'default-src': ['self'],
-				'connect-src': ['self'],
-				'img-src': ['self', 'data:', 'blob:'],
-				'style-src': ['self'],
-				'font-src': ['self'],
-				'script-src': ['self', 'trusted-types-eval', 'unsafe-eval'],
-				'object-src': ['none'],
-				'base-uri': ['none'],
-				'form-action': ['self'],
-				'frame-ancestors': ['none']
-			}
-		}
+		// csp: {
+		// 	mode: 'auto',
+		// 	directives: {
+		// 		'default-src': ['self'],
+		// 		'connect-src': ['self'],
+		// 		'img-src': ['self', 'data:', 'blob:'],
+		// 		'style-src': ['self'],
+		// 		'font-src': ['self'],
+		// 		'script-src': ['self', 'trusted-types-eval', 'unsafe-eval'],
+		// 		'object-src': ['none'],
+		// 		'base-uri': ['none'],
+		// 		'form-action': ['self'],
+		// 		'frame-ancestors': ['none']
+		// 	}
+		// }
 	},
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) => ({ runes: !filename.includes('node_modules') })
